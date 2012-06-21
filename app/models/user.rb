15e025002b656ee50f63aca2ddcbc0ffcde6ea37
Petr Zaparka <petr@zaparka.cz>
2012-06-21 12:18:47 +0100

    Annotation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  twitter    :string(255)
+#  uid        :string(255)
+#  provider   :string(255)
+#  runkeeper  :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 class User < ActiveRecord::Base
   attr_accessible :name, :provider, :runkeeper, :twitter, :uid
 end
