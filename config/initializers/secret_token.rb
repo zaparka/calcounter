d1688509f50e5f2c747de14a247290e5fc6f2f3e
Petr Zaparka <petr@zaparka.cz>
2012-06-21 12:02:06 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calcounter::Application.config.secret_token = 'e49365782a3c32389b043bec4df740495055a8957b999d4190900706ffd005d230979a6c0d27b4fd351e9114b88cc6cb98369704c3c4a1d95b91ca9776e12b8f'
