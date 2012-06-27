09fde74090c320021c6e252fa6fdd6e6dacf3776
Petr Zaparka <petr@zaparka.cz>
2012-06-27 16:36:55 +0100

    Improving ux

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,5 @@
 class Subscriber < ActiveRecord::Base
   validates_uniqueness_of :email
+  validates_format_of :email, :with => /\A([^@\s,]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   attr_accessible :email
 end
